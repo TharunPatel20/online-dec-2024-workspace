9d5499d7bf49a175279a650a06aa763b9f18dff3
bpoongs <bpoongs@gmail.com>
2025-01-31 20:53:21 +0530

    spring rest

--- spring-boot-mvc-demo-project/src/main/java/com/demo/spring_boot_mvc_demo_project/controller/GreetingController.java
+++ spring-boot-mvc-demo-project/src/main/java/com/demo/spring_boot_mvc_demo_project/controller/GreetingController.java
@@ -1,7 +1,9 @@
 package com.demo.spring_boot_mvc_demo_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
@@ -9,5 +11,12 @@ public class GreetingController {
 	@GetMapping("/greetings")
 	public String sayHelloEveryone() {
 		return "greet";
+	}
+	
+	// http://localhost:7575/greetname?username=John
+	@GetMapping("/greetname")
+	public String sayHelloName(@RequestParam(name="username", required = false, defaultValue = "World") String user, Model model) {
+		model.addAttribute("userinfo", user);
+		return "greetbyname";
 	}
 }
---
+++ spring-boot-rest-demo-project/src/main/java/com/demo/spring_boot_rest_demo_project/SpringBootRestDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_boot_rest_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestDemoProjectApplication.class, args);
+	}
+
+}
